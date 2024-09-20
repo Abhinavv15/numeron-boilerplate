f4f3099c4f01cabad377c06bce0527760bb74761
Abhinavv15 <abhinavdhanesh15@gmail.com>
2024-09-20 15:52:44 +0530

    it is working

--- score.js
+++ score.js
@@ -1,1 +1,8 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score=localStorage.getItem("scoreValue");
+document.getElementById("score-board").innerText=score;
+
+
+function startGame(){
+    location.href="./game.html";
+}
